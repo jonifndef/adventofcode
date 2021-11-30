e038880e620fc5f620e6e57b14033497fc59a4fb
Jonas Fredriksson <jonas.fredriksson92@gmail.com>
2021-11-30 21:45:37 +0100

    fixup! Add 2020/1 entry

--- 2020/1/main.cpp
+++ 2020/1/main.cpp
@@ -8,29 +8,59 @@
 
 std::unordered_map<std::string, std::string> parseArgs(int argc, char* argv[])
 {
-    int arg;
     std::unordered_map<std::string, std::string> arguments;
 
-    while ((arg = getopt(argc, argv, "i")) != -1)
+    const static std::string usage =
+        "Usage: aoc -i inputfile [-s solution_number]\n"
+        "\t-i inputfile: specify inputfile. Required\n"
+        "\t-s solution_number: specify solution number if multiple are available\n"
+        "\t-h: prints this message";
+
+    bool invalid = false;
+    int arg;
+    while ((arg = getopt(argc, argv, "i:sh")) != -1)
     {
         switch(arg)
         {
             case 'i':
-                arguments["input"] = optarg;
+                arguments["input"] = std::string(optarg);
+                break;
+            case 's':
+                arguments["solution"] = std::string(optarg);
+                break;
+            case 'h':
+                std::cout << usage << std::endl;
                 break;
             case '?':
+                if (optopt == 'i')
+                {
+                    std::cout << "Option -" << optopt << " requires an argument" << std::endl;
+                }
+                else
+                {
+                    invalid = true;
+                    std::cout << usage << std::endl;
+                }
+                break;
+            default:
+                break;
         }
     }
 
-    return
+    if (!invalid && !arguments.contains("input"))
+    {
+        std::cout << "Argument -i inputFile is required" << std::endl;
+    }
+
+    return arguments;
 }
 
-std::vector<int> getInputs()
+std::vector<int> getInputs(std::unordered_map<std::string, std::string> arguments)
 {
     std::vector<int> inputNumbers;
     std::ifstream inputFile;
 
-    inputFile.open("input", std::ios::in);
+    inputFile.open(arguments["input"], std::ios::in);
 
     std::string line;
     while (getline(inputFile, line))
@@ -42,56 +72,45 @@ std::vector<int> getInputs()
     return inputNumbers;
 }
 
-bool binarySearch(const std::vector<int>& inputNumbers,
-                  unsigned const int targetValue,
-                  unsigned int& lowerIndex,
-                  unsigned int& upperIndex)
+// A bit more optimized than just two nested for-loops
+bool search(const std::vector<int>& inputNumbers,
+            const int targetValue,
+            int& lowerIndex,
+            int& upperIndex)
 {
-    std::cout << "lowerIndex: " << lowerIndex << std::endl;
-    std::cout << "len of vector: " << inputNumbers.size() << lowerIndex << std::endl;
+    int lowerValue = inputNumbers[lowerIndex];
+    int upperValue = inputNumbers[upperIndex];
 
-    const unsigned int lowerValue = inputNumbers[lowerIndex];
-    const unsigned int upperValue = inputNumbers[upperIndex];
+    while (lowerValue + upperValue != targetValue)
+    {
+        if (lowerValue + upperValue > targetValue)
+        {
+            upperIndex--;
+        }
+        if (lowerValue + upperValue < targetValue)
+        {
+            lowerIndex++;
+        }
 
-    if (lowerValue + upperValue == targetValue)
-    {
-        return true;
-    }
-    else if (lowerValue + upperValue < targetValue)
-    {
-        const int diff = upperIndex - lowerIndex;
-        lowerIndex = diff / 2;
-
-        return binarySearch(inputNumbers,
-                            targetValue,
-                            lowerIndex,
-                            upperIndex);
-    }
-    else if (lowerValue + upperValue > targetValue)
-    {
-        upperIndex /= 2;
-        return binarySearch(inputNumbers,
-                            targetValue,
-                            lowerIndex,
-                            upperIndex);
+        lowerValue = inputNumbers[lowerIndex];
+        upperValue = inputNumbers[upperIndex];
     }
 
-    return false;
+    return true;
 }
 
-void getAnswer(const std::vector<int>& inputNumbers,
-               const unsigned int targetValue,
-               int& firstNum,
-               int& secondNum)
+void getPartOneAnswer_1(std::vector<int>& inputNumbers,
+                        int targetValue)
 {
+    std::sort(inputNumbers.begin(), inputNumbers.end());
 
-    unsigned int lowerIndex = 0;
-    unsigned int upperIndex = inputNumbers.size() - 1;
+    int lowerIndex = 0;
+    int upperIndex = inputNumbers.size() - 1;
 
-    if (binarySearch(inputNumbers,
-                     targetValue,
-                     lowerIndex,
-                     upperIndex))
+    if (search(inputNumbers,
+               targetValue,
+               lowerIndex,
+               upperIndex))
     {
         int lowerValue = inputNumbers[lowerIndex];
         int upperValue = inputNumbers[upperIndex];
@@ -101,20 +120,134 @@ void getAnswer(const std::vector<int>& inputNumbers,
     }
 }
 
-int main(int argc, char* argv[])
+void loopFunc(std::vector<int> inputNumbers,
+              const int targetValue,
+              int &lowerValue,
+              int &upperValue)
 {
-    std::vector<int> inputNumbers = getInputs();
-    std::sort(inputNumbers.begin(), inputNumbers.end());
+    for (int i = 0; i < (int)inputNumbers.size() - 1; i++)
+    {
+        for (int j = i + 1; j < (int)inputNumbers.size(); j++)
+        {
+            lowerValue = inputNumbers[i];
+            upperValue = inputNumbers[j];
 
-    int firstNum = 0;
-    int secondNum = 0;
-    int targetValue = 2000;
-    getAnswer(inputNumbers, targetValue, firstNum, secondNum);
+            if (lowerValue + upperValue == targetValue)
+            {
+                return;
+            }
+        }
+    }
+}
+
+// second, brute force nested for-loops solution for part one
+void getPartOneAnswer_2(std::vector<int>& inputNumbers,
+                        int targetValue)
+{
+    int lowerValue = 0;
+    int upperValue = 0;
+
+    loopFunc(inputNumbers, targetValue, lowerValue, upperValue);
+
+    std::cout << "lowerValue: " << lowerValue << std::endl;
+    std::cout << "upperValue: " << upperValue << std::endl;
+}
+
+void loopFunc_2(std::vector<int> inputNumbers,
+                const int targetValue,
+                int &firstIndex,
+                int &secondIndex,
+                int &thirdIndex)
+{
+    for (; firstIndex < secondIndex; firstIndex++)
+    {
+        secondIndex = firstIndex + 1;
+        for (; secondIndex < thirdIndex; secondIndex++)
+        {
+            std::cout << "firstIndex: " << firstIndex << std::endl;
+            std::cout << "secondIndex: " << secondIndex << std::endl;
+            std::cout << "thirdIndex: " << thirdIndex << std::endl;
+
+            if (inputNumbers[firstIndex] +
+                inputNumbers[secondIndex] +
+                inputNumbers[thirdIndex] == targetValue)
+            {
+                return;
+            }
+        }
+    }
+}
+
+// A bit more optimized than just three nested for-loops
+void solvePartTwo(std::vector<int> inputNumbers,
+                  const int targetValue,
+                  int &firstValue,
+                  int &secondValue,
+                  int &thirdValue)
+{
+    int firstIndex  = 0;
+    int secondIndex = 1;
+    int thirdIndex  = 2;
+
+    firstValue  = inputNumbers[firstIndex];
+    secondValue = inputNumbers[secondIndex];
+    thirdValue  = inputNumbers[thirdIndex];
+
+    while ((firstValue + secondValue + thirdValue < targetValue) &&
+           (thirdIndex < (int)inputNumbers.size()))
+    {
+        thirdIndex++;
+
+        thirdValue  = inputNumbers[thirdIndex];
+    }
+
+    loopFunc_2(inputNumbers, targetValue, firstIndex, secondIndex, thirdIndex);
+
+    std::cout << "- firstIndex: " << firstIndex << std::endl;
+    std::cout << "- secondIndex: " << secondIndex << std::endl;
+    std::cout << "- thirdIndex: " << thirdIndex << std::endl;
+
+    firstValue  = inputNumbers[firstIndex];
+    secondValue = inputNumbers[secondIndex];
+    thirdValue  = inputNumbers[thirdIndex];
+}
+
+void getPartTwoAnswer_1(std::vector<int>& inputNumbers,
+                        int targetValue)
+{
+    std::sort(inputNumbers.begin(), inputNumbers.end());
 
     //for (const int num : inputNumbers)
     //{
     //    std::cout << num << std::endl;
     //}
+
+    int firstValue  = 0;
+    int secondValue = 0;
+    int thirdValue  = 0;
+
+    solvePartTwo(inputNumbers, targetValue, firstValue, secondValue, thirdValue);
+
+    std::cout << "firstValue: "  << firstValue  << std::endl;
+    std::cout << "secondValue: " << secondValue << std::endl;
+    std::cout << "thirdValue: "  << thirdValue  << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+    auto arguments = parseArgs(argc, argv);
+    if (arguments.empty())
+    {
+        return 1;
+    }
+
+    std::vector<int> inputNumbers = getInputs(arguments);
+
+    int targetValue = 2020;
+
+    //getPartOneAnswer_1(inputNumbers, targetValue);
+    //getPartOneAnswer_2(inputNumbers, targetValue);
+    getPartTwoAnswer_1(inputNumbers, targetValue);
 
     return 0;
 }
